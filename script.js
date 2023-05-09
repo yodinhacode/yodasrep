bcfddcf0b636a5b90c8e3710119da5ae16eda79a
yodinhacode <132973641+yodinhacode@users.noreply.github.com>
2023-05-09 01:00:43 -0300

    commit.yodinha

---
+++ script.js
@@ -0,0 +1,9 @@
+var music = 'music'
+var img = "img"
+function clicar(){
+    window.alert('POR QUE NÃO CLARO?????')
+}
+
+function clicar2(){
+    location.href = 'https://www.youtube.com/watch?v=AamatUtxev4'
+}
